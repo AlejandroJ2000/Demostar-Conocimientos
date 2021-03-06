12067077aae6d352daac23b5411f1a6aaf75bbb4
AlejandroJ2000 <75826053+AlejandroJ2000@users.noreply.github.com>
2021-03-06 02:58:42 -0300

    Update code.js
     Ahora, cada una de las 12 cajas, que contienen toda información sobre comprar una camam no solo tendrán cada una la imagen específica a ese número de cama sino que también pueden ser seleccionada con un click. Dicho click dispara un "addEventListener" que guarda en un input oculto que número de modelo tiene la cama que el usuario selecciono, para así mandar la información a quien se ocupe de todo lo externo a la página en sí.

--- HTML + CSS + JS/code.js
+++ HTML + CSS + JS/code.js
@@ -1,24 +1,68 @@
 const contenedorGeneral = document.querySelector(".container");
 
-function ventaCama(nombre, numeroModelo, precio){
+function ventaCama(nombre, numeroModelo, precio, imagen){
     nombre = `<h3> Nombre: <b> ${nombre} </b> <br>`;
     numeroModelo = `<h4> <b> ${numeroModelo} </b> </h4>`;
     precio = `<h4> Coste: <b> ${precio}$ </b> </h4> <br>`;
-    imagen = `<img src="img/cama.jpg"> </img>`;
+    imagen = `<img src="img/${imagen}"> </img>`;
     return[nombre, numeroModelo, precio, imagen];
+}
+
+let modeloElegido = (number)=>{
+  entregarModelo = document.querySelector(".inputOculto");
+  entregarModelo.value = number;
 }
 
 const fragment = document.createDocumentFragment();
 
-for(i = 1; i <= 12; i++){
-  costePrecio = Math.round(Math.random()*100+10);
-  modelo = Math.round(Math.random()*1000000);
-  let cama = ventaCama(`Cama ${i}`, `Modelo: ${modelo}`, costePrecio);
+for(let i = 1; i <= 12; i++){
+  const costePrecio = Math.round(Math.random()*100+10);
+  const modelo = Math.round(Math.random()*1000000);
+  if(i == 1){
+    imagenSeleccionada = "cama-1.jpg";
+  }
+  else if(i == 2){
+    imagenSeleccionada = "cama-2.jpg";
+  }
+  else if(i == 3){
+    imagenSeleccionada = "cama-3.jpg";
+  }
+  else if(i == 4){
+    imagenSeleccionada = "cama-4.jpg";
+  }
+  else if(i == 5){
+    imagenSeleccionada = "cama-5.jpg";
+  }
+  else if(i == 6){
+    imagenSeleccionada = "cama-6.jpg";
+  }
+  else if(i == 7){
+    imagenSeleccionada = "cama-7.jpg";
+  }
+  else if(i == 8){
+    imagenSeleccionada = "cama-8.jpg";
+  }
+  else if(i == 9){
+    imagenSeleccionada = "cama-9.jpg";
+  }
+  else if(i == 10){
+    imagenSeleccionada = "cama-10.jpg";
+  }
+  else if(i == 11){
+    imagenSeleccionada = "cama-11.jpg";
+  }
+  else{
+    imagenSeleccionada = "cama-12.jpg";
+  }
+  let cama = ventaCama(`Cama ${i}`, `Modelo: ${modelo}`, costePrecio, imagenSeleccionada);
   let camasVenta = document.createElement("DIV");
+  camasVenta.addEventListener("click",()=>{modeloElegido(modelo)});
+  camasVenta.tabIndex = i;
   camasVenta.classList.add("container-cama", `cama-${i}`);
   camasVenta.innerHTML = cama[0] + cama[1] + cama[2] + cama[3];
   fragment.appendChild(camasVenta);
 }
 
 contenedorGeneral.appendChild(fragment);
+
 
